157de8417e0aebfb8eb1305c5e64f329d116e103
Michael Woods <mkwoods3@ncsu.edu>
2023-04-21 14:21:56 -0400

    Jason Website

---
+++ Web/src/app.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+$(function () {
+    $("#foot").load("foot.html");
+});
+$(function () {
+    $("#nav").load("nav.html");
+});
